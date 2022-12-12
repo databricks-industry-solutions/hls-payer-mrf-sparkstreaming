3c55afa375c4f9d545b89ef9593f74c349712195
dbbnicole <dbbnicole>
2022-12-12 23:30:29 +0000

    Downgrade DBR to 10.4 to match spark version

--- RUNME.py
+++ RUNME.py
@@ -79,7 +79,7 @@ job_json = {
             {
                 "job_cluster_key": "payer_mrf_cluster",
                 "new_cluster": {
-                    "spark_version": "11.3.x-scala2.12",
+                    "spark_version": "10.4.x-scala2.12",
                 "spark_conf": {
                     "spark.rpc.message.maxSize": "1024",
                     "spark.driver.memory": "12g",
