e6c657379b59c3065d770ceabd5381b49065f472
Aaron Zavora <aaron.zavora@databricks.com>
2022-12-14 14:41:57 -0500

    changing buffersize to 64MB

--- 01_payer_mrf_demo.py
+++ 01_payer_mrf_demo.py
@@ -47,7 +47,7 @@ spark.sql("""drop table if exists hls_dev_payer_transparency.payer_transparency_
 
 # COMMAND ----------
 
-df = spark.readStream.format("com.databricks.labs.sparkstreaming.jsonmrf.JsonMRFSourceProvider").load(source_data)
+df = spark.readStream.option("buffersize", 67108864).format("com.databricks.labs.sparkstreaming.jsonmrf.JsonMRFSourceProvider").load(source_data)
 query = (
 df.writeStream 
  .outputMode("append") 
